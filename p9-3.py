8f7966da9f96c9d708b9603325d94b66ecdfc6f7
J.MISAWA <jmisawa@JMISAWA-no-MacBook-Air.local>
2018-03-11 15:58:25 +0900

    課題9

--- p9-3.py
+++ p9-3.py
@@ -4,7 +4,12 @@ import numpy.linalg as la
 
 
 a = np.array([ [1,2],[3,4] ])
-b = np.array([ [1,2],[3,4] ])
+b = np.array([ [5,6],[7,8] ])
 
-for i in range(1):
-    print (a[i])
+for i in np.arange(1,2):
+   # print (a[i-1 , :  ])
+    c = (a[i-1 , :  ])
+#print(c)
+print(np.dot(c,b) )
+print('')
+print(np.dot(b,c) )
