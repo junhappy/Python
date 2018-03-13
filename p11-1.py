178de687ecab974e0bd5b3caa1d5749c21b4c800
Jun Misawa <misawa@YasuhironoiMac-2.local>
2018-03-13 14:59:31 +0900

    課題11

---
+++ p11-1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.linalg as la
+
+p = 3 #初期値x0
+
+#while true:
+
+   # a = x**3 - (3/2*x**2) -(3/2*x) + 1 #f(x)
+b = np.abs(-2)
+    
+print(b)
--- test.py
+++ test.py
@@ -1,26 +1,27 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import animation as ani
+#https://raw.githubusercontent.com/PrinzEugen7/Lesson/master/Python/math/newton_method.py
+# 解を求める関数
+def f(x):
+    return  x**3 - (3/2*x**2) -(3/2*x) + 1 #f(x)
+ 
+# 導関数
+def df(x):
+    return 2.0*x
 
-plt.figure(figsize=(10,10))
-n=10
-xmin = 0
-xmax = 8
-ymin = 0
-ymax = 8
-
-A = [[ 3, 2],
-     [5,2]]
-for i in range(n):
-    for j in range(n):
-        x=j
-        y=i
-        a = np.dot(A, [x, y])
-        #plt.scatter(x,  y,  facecolor="b", edgecolors='none', alpha=.7, s=20)
-        plt.scatter(a[i], a[i], facecolor="r", edgecolors='none', alpha=.7)
-
-        #plt.plot([xmin,xmax],[0,0],"k", linewidth=1)
-        #plt.plot([0,0],[ymin,ymax],"k", linewidth=1)
-        plt.xlim(xmin, xmax)
-        plt.ylim(ymin, ymax)
-plt.show()
+# ニュートン法
+def newton_method(a, eps):
+    for i in range(1000):
+          # 漸化式
+        ah = a - f(a)/df(a)
+        # 収束条件(近似解の変化が十分小さい)を満たせば計算終了
+        if abs(ah - a) < eps:break
+        #　近似解の更新
+        a = ah      
+    return a, i
+    
+    
+def main():
+    a , i = newton_method(1.0, 0.0001)
+    print("解:",a ,"(計算回数:", i+1, ")") # 解: 2.00000... (計算回数: 5 )
+    
+if __name__ == '__main__':
+    main()
